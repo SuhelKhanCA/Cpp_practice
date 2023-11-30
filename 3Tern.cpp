d1738be9014985b5d992d505f77680dcc40f3882
Suhel Khan <112371384+SuhelKhanCA@users.noreply.github.com>
2023-11-30 23:51:50 +0530

    Add files via upload

---
+++ 3Tern.cpp
@@ -0,0 +1,12 @@
+#include<iostream>    
+ 
+using namespace std;
+int main()
+{ 
+	int a, b, c;
+	cin>>a>>b>>c;
+	
+	a>b? a>c? cout<<"A is": cout<<"c is":b>c?cout<<"B is": cout<<"C is";
+   
+   return 0;
+}
